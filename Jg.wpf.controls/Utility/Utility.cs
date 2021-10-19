53b7a1a61e521d19709a79f7979e4c4b1cd634a5
Fei Zhu <zfgogogo@163.com>
2021-10-19 15:52:33 +0800

    新增： DispatcherHelper

---
+++ Jg.wpf.controls/Utility/Utility.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Threading;
+
+namespace Jg.wpf.controls.Utility
+{
+    public static class DispatcherHelper
+    {
+        public static void DoEvents()
+        {
+            var nestedFrame = new DispatcherFrame();
+            var exitOperation = Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background, new DispatcherOperationCallback(ExitFrame), nestedFrame);
+
+            Dispatcher.PushFrame(nestedFrame);
+            if (exitOperation.Status != DispatcherOperationStatus.Completed)
+            {
+                exitOperation.Abort();
+            }
+        }
+
+        private static object ExitFrame(object arg)
+        {
+            ((DispatcherFrame)arg).Continue = false;
+            return null;
+        }
+    }
+}
